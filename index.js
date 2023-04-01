35f2902737e07819891230c80a6dda798e32672e
Franklin Hau <fhauchi@gmail.com>
2023-04-01 16:56:44 -0600

    filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers, searchDriver) {
+    return drivers.filter(driver => driver.toLowerCase() === searchDriver.toLowerCase());
+  }
+function fuzzyMatch(drivers, searchDriver) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(searchDriver.toLowerCase()));
+  }
+  function matchName(drivers, searchDriver) {
+    return drivers.filter(driver => driver.name.toLowerCase() === searchDriver.toLowerCase());
+  }
